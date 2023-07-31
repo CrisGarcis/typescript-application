fb83780b9b0d2de0d771fcd000c6e52372de27fd
CrisGarcis <jhonier491@gmail.com>
2023-07-31 08:25:21 -0500

    first commmit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { Fragment, useState, useRef } from "react";
+type FormElement = React.FormEvent<HTMLFormElement>;
+interface TaskState {
+  name: string;
+  done: boolean;
+}
+function App(): JSX.Element {
+  const [newTask, setNewTask] = useState("");
+  const [tasks, setTasks] = useState<Array<TaskState>>([]);
+  const taskInput = useRef<HTMLInputElement>(null);
+  const handleSubmit = (e: FormElement) => {
+    e.preventDefault();
+    addTask(newTask);
+    setNewTask("");
+  };
+  const addTask = (name: string): void => {
+    setTasks([...tasks, { name, done: false }]);
+    taskInput.current?.focus();
+  };
+  const toogleDoneTask = (i: number) => {
+    const newTasks: TaskState[] = [...tasks];
+    newTasks[i].done = !newTasks[i].done;
+    setTasks(newTasks);
+  };
+  const removeTask = (i: number): void => {
+    const newTasks: TaskState[] = [...tasks];
+    newTasks.splice(i, 1);
+    setTasks(newTasks);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setNewTask(e.target.value)}
+          value={newTask}
+          autoFocus
+          ref={taskInput}
+        />
+        <button>save</button>
+      </form>
+      {tasks.map((task: TaskState, i) => {
+        return (
+          <div>
+            <h1>{task.name}</h1>
+            <button onClick={() => toogleDoneTask(i)}>
+              {task.done ? "hecha" : "pendiente"}
+            </button>
+            <button onClick={() => removeTask(i)}>eliminar</button>
+          </div>
+        );
+      })}
+    </Fragment>
   );
 }
 
